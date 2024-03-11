06cb222572321f23bb16420c671034d26c515fa6
Mohnish <mohnish.a007@gmail.com>
2024-03-11 21:20:46 +0000

    Express routes setup successfully

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const app = express()
 const path= require('path')
-const port = 3000
+const port = 3400
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
+app.use(express.static(path.join(__dirname, 'static')));
+app.use('/', require(path.join(__dirname,'routes/blog.js')))
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
