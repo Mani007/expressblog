9b10aed0855e155e9bab009815e3c5e3022760dc
Mohnish <mohnish.a007@gmail.com>
2024-03-12 00:43:47 +0000

    Express basic blog setup dome

--- routes/blog.js
+++ routes/blog.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const path = require('path')
+const blogs = require('../data/blog')
 
 router.get('/', (req,res,next) => {
     res.sendFile(path.join(__dirname,'../templates/index.html'))
+})
+
+router.get('/blog', (req,res,next) => {
+    blogs.forEach(e => {
+        console.log(e.title)
+    });
+    res.sendFile(path.join(__dirname,'../templates/bloghome.html'))
 })
 
 module.exports = router
